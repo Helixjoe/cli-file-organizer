fd1af81818399fe35be05d70027b10d56e0a43fb
Helixjoe <joeljohn5112@gmail.com>
2026-03-21 15:45:12 +0530

    basic menu

--- main.go
+++ main.go
@@ -7,13 +7,25 @@ import (
 )
 
 func main() {
-	fmt.Printf("Directory Scanning \n")
-	files, err := os.ReadDir(".")
-	if err != nil {
-		log.Fatal(err)
+	fmt.Printf("Welcome to helixjoe's file organizer. We provide..\n")
+	fmt.Printf("--------------------------- \n")
+	fmt.Printf(" 1. Directory Scanning \n")
+	fmt.Printf("--------------------------- \n")
+	fmt.Printf("Enter an option : ")
+	var option int
+	fmt.Scan(&option)
+	switch option {
+	case 1:
+		files, err := os.ReadDir(".")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, file := range files {
+			fmt.Println(file.Name(), file.Type(), file.IsDir())
+		}
+	default:
+		fmt.Println("Choose a better option")
 	}
 
-	for _, file := range files {
-		fmt.Println(file.Name(), file.Type(), file.IsDir())
-	}
 }
